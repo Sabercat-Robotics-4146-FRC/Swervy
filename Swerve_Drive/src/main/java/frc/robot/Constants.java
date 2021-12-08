e79cde013a8a4536fab0e6a1c7cd2f6f7dceec47
Lucas Lesser <lesser618@gmail.com>
2021-12-08 09:37:48 -0700

    SDS swerve added

--- Swerve_Drive/src/main/java/frc/robot/Constants.java
+++ Swerve_Drive/src/main/java/frc/robot/Constants.java
@@ -1,40 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot;
 
-public class Constants {
-  public static final double kLooperDt = 0.01;
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  /**
+   * The left-to-right distance between the drivetrain wheels
+   *
+   * <p>Should be measured from center to center.
+   */
+  public static final double DRIVETRAIN_TRACKWIDTH_METERS = 1.0; // FIXME Measure and set trackwidth
+  /**
+   * The front-to-back distance between the drivetrain wheels.
+   *
+   * <p>Should be measured from center to center.
+   */
+  public static final double DRIVETRAIN_WHEELBASE_METERS = 1.0; // FIXME Measure and set wheelbase
 
-  // CAN
-  public static final int kLongCANTimeoutMs = 100; // use for constructors
-  public static final int kCANTimeoutMs = 10; // use for important on the fly updates
-  public static final int kPigeonID = 21;
+  public static final int DRIVETRAIN_PIGEON_ID = 0; // FIXME Set Pigeon ID
 
-  // Drive
-  public static final int kFRSpeedId = 1;
-  public static final int kFROrientationId = 2;
-  public static final int kFLSpeedId = 3;
-  public static final int kFLOrientationId = 4;
-  public static final int kBRSpeedId = 5;
-  public static final int kBROrientationId = 6;
-  public static final int kBLSpeedId = 7;
-  public static final int kBLOrientationId = 8;
-  public static final int kFRCANCoderId = 9;
-  public static final int kFLCANCoderId = 10;
-  public static final int kBRCANCoderId = 11;
-  public static final int kBLCANCoderId = 12;
-  public static final int kFLPidId = 1;
-  public static final int kFRPidId = 2;
-  public static final int kBLPidId = 3;
-  public static final int kBRPidId = 4;
-  public static final int kTimeout = 30;
-  public static final double kWheelDistanceW = 16.8125;
-  public static final double kWheelDistanceL = 23.5;
-  public static final double kR =
-      Math.sqrt((kWheelDistanceL * kWheelDistanceL) + (kWheelDistanceW * kWheelDistanceW));
-  public static final double kPGain = .0005;
-  public static final double kIGain = 0;
-  public static final double KDGain = 0;
+  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR =
+      0; // FIXME Set front left module drive motor ID
+  public static final int FRONT_LEFT_MODULE_STEER_MOTOR =
+      0; // FIXME Set front left module steer motor ID
+  public static final int FRONT_LEFT_MODULE_STEER_ENCODER =
+      0; // FIXME Set front left steer encoder ID
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET =
+      -Math.toRadians(0.0); // FIXME Measure and set front left steer offset
 
-  // Xbox Controllers
-  public static final int kDriver1USBPort = 0;
-  public static final double kJoystickThreshold = 0.2;
+  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR =
+      0; // FIXME Set front right drive motor ID
+  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR =
+      0; // FIXME Set front right steer motor ID
+  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER =
+      0; // FIXME Set front right steer encoder ID
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET =
+      -Math.toRadians(0.0); // FIXME Measure and set front right steer offset
+
+  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 0; // FIXME Set back left drive motor ID
+  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 0; // FIXME Set back left steer motor ID
+  public static final int BACK_LEFT_MODULE_STEER_ENCODER =
+      0; // FIXME Set back left steer encoder ID
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET =
+      -Math.toRadians(0.0); // FIXME Measure and set back left steer offset
+
+  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 0; // FIXME Set back right drive motor ID
+  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 0; // FIXME Set back right steer motor ID
+  public static final int BACK_RIGHT_MODULE_STEER_ENCODER =
+      0; // FIXME Set back right steer encoder ID
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET =
+      -Math.toRadians(0.0); // FIXME Measure and set back right steer offset
 }
